7c1ecb4d610d5422926d9a2fc320b037634a4878
Alexander Konstantinov <konstantinov@gmail.com>
2011-08-29 14:27:30 +0200

    Added method to fill list of tasks; added stub method to create task

--- src/com/k10v/goaltracker/Main.java
+++ src/com/k10v/goaltracker/Main.java
@@ -1,9 +1,11 @@
 package com.k10v.goaltracker;
 
 import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
 
 public class Main extends ListActivity {
 
@@ -18,11 +20,11 @@ public class Main extends ListActivity {
         setContentView(R.layout.main);
         mDbHelper = new GoalTrackerDbAdapter(this);
         mDbHelper.open();
+        fillTasksList();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // TODO Auto-generated method stub
         boolean result = super.onCreateOptionsMenu(menu);
         menu.add(0, MENU_ID_ADD_TASK, 0, R.string.menu_add_task);
         return result;
@@ -30,7 +32,29 @@ public class Main extends ListActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+        case MENU_ID_ADD_TASK:
+            createTask();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createTask() {
+        // ... Create task
+        fillTasksList();
+    }
+
+    private void fillTasksList() {
+        Cursor c = mDbHelper.getTaskPeer().fetchAllTasks();
+        startManagingCursor(c);
+
+        String[] from = new String[] { TaskPeer.KEY_TITLE };
+        int[] to = new int[] { R.id.tasks_row_text };
+
+        SimpleCursorAdapter tasks = new SimpleCursorAdapter(this,
+                R.layout.tasks_row, c, from, to);
+        setListAdapter(tasks);
     }
 }
