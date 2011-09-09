ffe5371cc7bbf3ed4893157a78a2916bc04b0a6c
Alexander Konstantinov <konstantinov@gmail.com>
2011-09-09 08:30:57 +0200

    Changed visual style of the task list

--- gen/com/k10v/goaltracker/R.java
+++ gen/com/k10v/goaltracker/R.java
@@ -18,7 +18,8 @@ public final class R {
         public static final int task_start_value=0x7f050001;
         public static final int task_target_value=0x7f050002;
         public static final int task_title=0x7f050000;
-        public static final int tasks_row_text=0x7f050004;
+        public static final int tasks_row_container=0x7f050004;
+        public static final int tasks_row_text=0x7f050005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
