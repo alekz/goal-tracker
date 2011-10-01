1d0aadd08ce9fdf1b962cf99f73b929607d341d0
Alexander Konstantinov <konstantinov@gmail.com>
2011-10-01 21:02:01 +0200

    Added more details to a chart (axes and colors)

--- src/com/k10v/goaltracker/Panel.java
+++ src/com/k10v/goaltracker/Panel.java
@@ -22,7 +22,7 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
     private Date mMaxDate;
 
     private Float mStartValue = null;
-    private Float mFinishValue = null;
+    private Float mLastValue = null;
     private Float mTargetValue = null;
     private Float mMinValue = null;
     private Float mMaxValue = null;
@@ -65,14 +65,25 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void onDraw(Canvas canvas) {
 
-        Paint paint = new Paint();
-        paint.setARGB(255, 255, 255, 255);
+        // Prepare line colors for different parst of the graph
 
-        Calendar cal = Calendar.getInstance();
-        Date date = mMinDate;
+        Paint paintProgress = new Paint();
+        paintProgress.setARGB(255, 64, 255, 64);
 
+        Paint paintAxes = new Paint();
+        paintAxes.setARGB(255, 128, 128, 128);
+
+        Paint paintCurrent = new Paint();
+        paintCurrent.setARGB(255, 0, 64, 0);
+
+        Paint paintDays = new Paint();
+        paintDays.setARGB(255, 32, 32, 32);
+
+        // Dimensions of the paint area
         int width = canvas.getWidth();
         int height = canvas.getHeight();
+        int xMax = width - 1;
+        int yMax = height - 1;
 
         // If task has a target value which is not reached yet, use target value
         // as a maximum value; also use today as a maximum date
@@ -90,17 +101,26 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
             maxDate = calendar.getTime();
         }
 
+        // Ranges of dates and values
         // TODO: Use JodaTime library for dates instead?
         int dateRange = 1 + Math.round((maxDate.getTime() - mMinDate.getTime()) / (24 * 60 * 60 * 1000f));
         float valueRange = maxValue - mMinValue;
 
-        int i = 0;
-        float value = mStartValue;
-        int x0 = 0;
-        int y0 = Math.round((height - 1) * (value / valueRange));
+        // Draw axes
+        int startValueY = Math.round(yMax * (mStartValue / valueRange));
+        int lastValueY = Math.round(yMax * (mLastValue / valueRange));
+        canvas.drawLine(0, yMax - lastValueY, xMax, yMax - lastValueY, paintCurrent);
+        canvas.drawLine(0, yMax - startValueY, xMax, yMax - startValueY, paintAxes);
+        canvas.drawLine(0, 0, 0, yMax, paintAxes);
 
-        // while (maxDate >= date)
-        while (maxDate.compareTo(date) >= 0) {
+        // Cycle through all dates
+        Calendar cal = Calendar.getInstance();
+        Date date = mMinDate;
+        float value = mStartValue;
+        int i = 0;
+        int x0 = 0;
+        int y0 = Math.round(yMax * (value / valueRange));
+        while (maxDate.compareTo(date) >= 0) { // while (maxDate >= date)
 
             i++;
 
@@ -109,10 +129,14 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
                 value = mValues.get(date);
             }
 
-            int x = (width - 1) * i / dateRange;
-            int y = Math.round((height - 1) * (value / valueRange));
+            int x = xMax * i / dateRange;
+            int y = Math.round(yMax * (value / valueRange));
 
-            canvas.drawLine(x0, height - y0 - 1, x, height - y - 1, paint);
+            // Draw vertical line
+            canvas.drawLine(x, 0, x, yMax, paintDays);
+
+            // Draw progress
+            canvas.drawLine(x0, yMax - y0, x, yMax - y, paintProgress);
 
             // Next date
             cal.setTime(date);
@@ -135,7 +159,7 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
         // Get information about the task
         mMinValue = mMaxValue = mStartValue = taskCursor.getFloat(
                 taskCursor.getColumnIndexOrThrow(TaskPeer.KEY_START_VALUE));
-        mFinishValue = mTargetValue = taskCursor.getFloat(
+        mLastValue = mTargetValue = taskCursor.getFloat(
                 taskCursor.getColumnIndexOrThrow(TaskPeer.KEY_TARGET_VALUE));
 
         // Process list of reports
@@ -172,15 +196,13 @@ public class Panel extends SurfaceView implements SurfaceHolder.Callback {
                 mMaxValue = value;
             }
 
+            mLastValue = value;
+
             if (mMinDate == null) {
                 mMinDate = date;
             }
 
             mMaxDate = date;
-        }
-
-        if (mFinishValue == null) {
-            mFinishValue = mMaxValue;
         }
     }
 }
