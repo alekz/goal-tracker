7307ff4d51be90b842518e80019194b452fb60d6
Alexander Konstantinov <konstantinov@gmail.com>
2011-10-03 18:58:37 +0200

    Added menu to TaskGraph activity

--- src/com/k10v/goaltracker/TaskGraph.java
+++ src/com/k10v/goaltracker/TaskGraph.java
@@ -1,13 +1,22 @@
 package com.k10v.goaltracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class TaskGraph extends Activity {
 
     private static final String TAG = "TaskGraph";
+
+    private static final int ACTIVITY_CREATE_REPORT = 0;
+    private static final int ACTIVITY_VIEW_TASK_REPORTS = 1;
+
+    public static final int MENU_ID_CREATE_REPORT = Menu.FIRST;
+    public static final int MENU_ID_VIEW_TASK_REPORTS = Menu.FIRST + 1;
 
     private GoalTrackerDbAdapter mDbHelper;
     private Cursor mTaskCursor;
@@ -62,6 +71,66 @@ public class TaskGraph extends Activity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putSerializable(TaskPeer.KEY_ID, mTaskId);
+    }
+
+    /**
+     * Create main menu.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        boolean result = super.onCreateOptionsMenu(menu);
+        menu.add(0, MENU_ID_CREATE_REPORT, 0, R.string.menu_create_report);
+        menu.add(0, MENU_ID_VIEW_TASK_REPORTS, 0, R.string.menu_view_task_reports);
+        return result;
+    }
+
+    /**
+     * Main menu item clicked
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+        case MENU_ID_CREATE_REPORT:
+            runCreateReport();
+            return true;
+
+        case MENU_ID_VIEW_TASK_REPORTS:
+            runViewTaskReports(mTaskId);
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Process result from other activity (create report etc.)
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        drawGraph();
+    }
+
+    /**
+     * Runs "Create Report" activity
+     */
+    private void runCreateReport() {
+        Intent i = new Intent(this, ReportEdit.class);
+        i.putExtra(ReportPeer.KEY_TASK_ID, mTaskId);
+        startActivityForResult(i, ACTIVITY_CREATE_REPORT);
+    }
+
+    /**
+     * Displays list of task reports
+     *
+     * @param rowId
+     */
+    private void runViewTaskReports(long rowId) {
+        Intent i = new Intent(this, ReportList.class);
+        i.putExtra(TaskPeer.KEY_ID, rowId);
+        startActivityForResult(i, ACTIVITY_VIEW_TASK_REPORTS);
     }
 
     /**
